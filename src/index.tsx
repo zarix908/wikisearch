93c29801e6a9ceda9e28ecb19bc7568f725e32c2
zarix908 <daniyarrus@gmail.com>
2019-03-19 14:20:07 +0500

    fix serviceWorker bug

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { CookiesProvider } from "react-cookie";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
-import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
@@ -14,8 +13,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
